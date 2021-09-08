56bef7e6ff05d7a109f445893d409d780f62e860
Ramkumar More <ram.more@csystech.com>
2021-09-08 13:27:42 +0530

    Added child parent component code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent implements OnInit {
+  @ViewChild('myDiv') myDiv: ElementRef;
+  showEditForm = false;
+
+  serverList: { id: number, name: string, ipAddress: string }[] = [
+    {
+      id: 0,
+      name: 'Server 1',
+      ipAddress: '10.20.2.225'
+    },
+    {
+      id: 1,
+      name: 'Server 2',
+      ipAddress: '10.20.2.200'
+    },
+    {
+      id: 2,
+      name: 'Server 3',
+      ipAddress: '10.20.2.150'
+    },
+    {
+      id: 3,
+      name: 'Server 4',
+      ipAddress: '10.20.2.80'
+    }
+  ];
+
+  selectedServer: { id: number, name: string, ipAddress: string } = { id: null, name: '', ipAddress: '' }
+
+  ngOnInit(): void {
+  }
+
+  onSelectServer(server) {
+    this.showEditForm = true;
+    this.selectedServer = server;
+  }
+
+  onServerUpdated(event) {
+    const id = event.id;
+
+    this.serverList.forEach(server => {
+      if (server.id === id) {
+        server.ipAddress = event.ipAddress;
+        server.name = event.name;
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ServerListComponent } from './server-list/server-list.component';
+import { AddServerComponent } from './add-server/add-server.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerListComponent,
+    AddServerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
