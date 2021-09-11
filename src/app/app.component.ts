f5ed4429e427a8df9de2a0edba216a7c08035db7
Ramkumar More <ram.more@csystech.com>
2021-09-11 10:34:56 +0530

    Added pipe code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,56 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  userList: { id: number, name: string; age: number, status: boolean }[] = [
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('myInput', {read: ElementRef}) myInput: ElementRef;
+
+  userList: { id: number, name: string; age: number, birthDate: Date, status: boolean }[] = [
     {
       id: 0,
       name: 'Ganesh',
       age: 29,
-      status: false
+      status: false,
+      birthDate: new Date('1996-07-29')
     },
     {
       id: 0,
       name: 'Kunal',
       age: 29,
-      status: true
+      status: true,
+      birthDate: new Date('1994-07-29')
     },
     {
       id: 0,
       name: 'Dhruva',
       age: 29,
-      status: false
+      status: false,
+      birthDate: new Date('1992-07-05')
     },
     {
       id: 0,
       name: 'Kalyani',
       age: 29,
-      status: true
+      status: true,
+      birthDate: new Date('1880-07-29')
     },
     {
       id: 0,
       name: 'Mahesh',
       age: 29,
-      status: true
+      status: true,
+      birthDate: new Date('1995-02-21')
     },
   ];
 
-  selectedUser: { id: number, name: string, age: number };
+  selectedUser: { id: number, name: string, age: number, status: boolean, birthDate: Date };
   // isUserDataAvailable = false;
 
   ngOnInit(): void {
+    console.log(this.myInput);
     // this.isUserDataAvailable = true;
     // // failed to fetch data from backend
     // setTimeout(() => {
@@ -50,7 +58,19 @@ export class AppComponent implements OnInit {
     // }, 10000);
   }
 
+  ngAfterViewInit(): void {
+    console.log(this.myInput);
+  }
+
   getFirstLetter(name): string {
     return name.toString()[0];
+  }
+
+  onEditUser(user): void {
+    this.selectedUser = user;
+  }
+
+  onHideUserForm(): void {
+    this.selectedUser = null;
   }
 }
--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,18 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.css']
 })
-export class EditUserComponent implements OnInit {
-  @Input() user: { id: number, name: string, age: number };
+export class EditUserComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() user: { id: number, name: string, age: number, status: boolean, birthDate: Date };
 
-  inputUser: { id: number, name: string, age: number };
+  inputUser: { id: number, name: string, age: number, status: boolean, birthDate: Date } = {
+    id: 0, name: '', age: 0, status: false, birthDate: new Date('01-01-1996')
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Rendered with initialization');
+    this.inputUser.id = this.user.id;
+    this.inputUser.name = this.user.name;
+    this.inputUser.age = this.user.age;
+    this.inputUser.status = this.user.status;
+    this.inputUser.birthDate = this.user.birthDate;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Rendered with changes');
+    if (changes.user) {
+      this.inputUser.id = this.user.id;
+      this.inputUser.name = this.user.name;
+      this.inputUser.age = this.user.age;
+      this.inputUser.status = this.user.status;
+      this.inputUser.birthDate = this.user.birthDate;
+    }
+  }
+
+  ngOnDestroy(): void {
+    console.log('On Destroy called');
   }
 
 }
